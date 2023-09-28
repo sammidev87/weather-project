74cf36e660752f41e119a857e9abe9a891ac703b
sammidev87 <sammidev87@gmail.com>
2023-09-28 01:41:52 -0500

    removed bootstrap, ajax, and jquery. Replaced older images with newer, and added new icons

--- js/main.js
+++ js/main.js
@@ -1,56 +1,70 @@
+const cityCoordinatesAPI = './city_coordinates.json';
+const display = document.querySelector('#display');
 
-///////////////////////////////////////////////////////////////
-// clock function
-const displayTime = document.querySelector(".display-time");
-// Time
-function showTime() {
-  let time = new Date();
-  displayTime.innerText = time.toLocaleTimeString("en-US", { hour12: false });
-  setTimeout(showTime, 1000);
+const getLocationData = async () => {
+    const data = await fetch(cityCoordinatesAPI);
+    const jsonData = await data.json();
+    return jsonData;
 }
 
-showTime();
-
-// Date
-function updateDate() {
-  let today = new Date();
-
-  // return number
-  let dayName = today.getDay(),
-    dayNum = today.getDate(),
-    month = today.getMonth(),
-    year = today.getFullYear();
-
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  const dayWeek = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  // value -> ID of the html element
-  const IDCollection = ["day", "daynum", "month", "year"];
-  // return value array with number as a index
-  const val = [dayWeek[dayName], dayNum, months[month], year];
-  for (let i = 0; i < IDCollection.length; i++) {
-    document.getElementById(IDCollection[i]).firstChild.nodeValue = val[i];
-  }
+const getCityData = async (cityName) => {
+    const payload = await getLocationData();
+    const filteredPayload = payload.filter((cityData) => {
+        if (cityData.city == cityName) {
+            const data = {
+                "city": cityData.city,
+                "country": cityData.country,
+                "latitude": cityData.latitude,
+                "longitude": cityData.longitude
+            }
+            return data;
+        } else {
+            return
+        }
+    });
+    return filteredPayload;
 }
 
-updateDate();
+const getWeatherData = async (cityName) => {
+    const filteredPayload = await getCityData(cityName);
+    const res = await fetch(`http://www.7timer.info/bin/api.pl?lon=${filteredPayload[ 0 ].longitude}&lat=${filteredPayload[ 0 ].latitude}&product=civillight&output=json`);
+    const data = await res.json();
+    return data.dataseries;
+}
+
+const displayWeatherData = async (cityName) => {
+    const weatherData = await getWeatherData(cityName);
+    const dataDisplay = weatherData.map((object) => {
+        const { date, weather, temp2m, wind10m_max } = object;
+        var convertedDate = date.toString().replace(/(\d\d\d\d)(\d\d)(\d\d)/g, '$1/$2/$3');
+
+        return `
+        <img src="./images/weather/${weather}.png" class="weather-icon">
+        <h1 class="date">${convertedDate}</h1>
+        <h2 class="city">${cityName}</h2>
+        <div class="details">
+            <div class="col1">
+                <img src="./images/icons/icons8-thermometer-100.png">
+                <div>
+                    <p class="tempMax">${temp2m.max}</p>
+                    <p>Max Temp</p>
+                </div>
+                <div>
+                    <p class="tempMin">${temp2m.min}</p>
+                    <p>Min Temp</p>
+                </div>
+            </div>
+            <div class="col2">
+                <img src="./images/icons/icons8-wind-100.png">
+                <div>
+                    <p class="windy">${wind10m_max} kph</p>
+                    <p>Wind Speed</p>
+                </div>
+            </div>
+        </div>`;
+    })
+
+    display.innerHTML = dataDisplay;
+}
+
+displayWeatherData("Amsterdam");
